3a1ba2273b6d0dab941739a23dc26bb866e151e4
wb-jzn506281@antfin.com <wb-jzn506281@antfin.com>
2019-05-19 19:47:07 +0800

    main.js添加示例代

--- vuex-explanation/src/main.js
+++ vuex-explanation/src/main.js
@@ -1,8 +1,54 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Vuex from 'vuex';
 
 Vue.config.productionTip = false
 
+Vue.use(Vuex);
+
+const moduleA = {
+  namespaced: true,
+  state: {
+    count: 1
+  },
+  mutations: {
+    add(state) {
+      state.count++
+    }
+  },
+  actions: {
+    actionAdd(ctx){
+      ctx.commit('add');
+    }
+  },
+  getters: {
+    compenedCount(state) {
+      return state.count + 1
+    }
+  }
+};
+
+const moduleB = {
+
+};
+
+
+const store = new Vuex.Store({
+  modules: {
+    a: moduleA,
+    b: moduleB,
+  },
+  state: {
+    count: 1,
+  },
+
+});
+
+
+
+
+
 new Vue({
   render: h => h(App),
+  store,
 }).$mount('#app')
