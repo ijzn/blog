5c8d0fe38470780cf649eb9c3cc5b147e46982cf
ijzn <592193834@qq.com>
2020-01-05 14:56:34 +0800

    doc：钩子函数

--- jest/lesson6/README.md
+++ jest/lesson6/README.md
@@ -1,6 +1,21 @@
 # jest 钩子函数
+核心的4个钩子函数：
+beforeAll afterAll
+beforeEach afterEach
 
+分组 describe
 
 ## beforeAll
+所有测试用例执行前，会执行此钩子函数
 
 ## afterAll
+所有测试用例执行完毕后，会执行此钩子函数
+
+## beforeEach
+每次执行测试用例前都会执行此钩子函数；
+实例类要在这个钩子函数中进行。
+
+## afterEach
+每次执行测试用例后都会执行此钩子函数
+
+详情请见 代码
---
+++ jest/lesson6/conter.test.js
@@ -0,0 +1,51 @@
+import { Conter } from './conter';
+
+let conter = null;
+
+describe('加法相关', () => {
+    test('+1', () => {
+        console.log("+1");
+        conter.addOne()
+        expect(conter.number).toBe(1)
+    })
+    test('+2', () => {
+        console.log("+2");
+        conter.addTwo()
+        expect(conter.number).toBe(2)
+    })
+})
+
+describe('减法相关', () => {
+    test('-1', () => {
+        console.log('-1');
+        conter.minuOne();
+        expect(conter.number).toBe(-1)
+    })
+    test('-2', () => {
+        console.log('-2');
+        conter.minuTwo();
+        expect(conter.number).toBe(-2)
+    })
+})
+
+
+
+
+beforeAll(() => {
+    console.log('beforeAll');
+})
+
+afterAll(() => {
+    console.log('afterAll');
+})
+
+// 每次执行测试用例前都会执行此钩子函数
+beforeEach(() => {
+    conter = new Conter();
+    console.log('beforeEach');
+})
+
+// 每次执行测试用例前都会执行此钩子函数
+afterEach(() => {
+    console.log('afterEach');
+})
