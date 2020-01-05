da1cb74fd04186aa24e56485e2d0c25b294a7c12
ijzn <592193834@qq.com>
2020-01-05 15:14:25 +0800

    feat: 钩子函数作用域

--- jest/lesson6/README.md
+++ jest/lesson6/README.md
@@ -1,21 +1,30 @@
-# jest 钩子函数
+## jest 钩子函数
 核心的4个钩子函数：
 beforeAll afterAll
 beforeEach afterEach
 
 分组 describe
 
-## beforeAll
+### beforeAll
 所有测试用例执行前，会执行此钩子函数
 
-## afterAll
+### afterAll
 所有测试用例执行完毕后，会执行此钩子函数
 
-## beforeEach
+### beforeEach
 每次执行测试用例前都会执行此钩子函数；
 实例类要在这个钩子函数中进行。
 
-## afterEach
+### afterEach
 每次执行测试用例后都会执行此钩子函数
 
 详情请见 代码
+
+## jest 钩子函数的作用域
+钩子函数的作用域在最近的一个descibe中.
+执行顺序是由外到内的。
+
+
+## tips 调试技巧
+1. test.only 只执行一个测试用例，忽略其他。
+2. 所有准备代码一定要放在钩子函数里面。重点！！！ 否则排查问题能被坑死
--- jest/lesson6/conter.test.js
+++ jest/lesson6/conter.test.js
@@ -2,50 +2,67 @@ import { Conter } from './conter';
 
 let conter = null;
 
-describe('加法相关', () => {
-    test('+1', () => {
-        console.log("+1");
-        conter.addOne()
-        expect(conter.number).toBe(1)
+describe('conter test', () => {
+
+
+    console.log('discribe 1');
+    beforeAll(() => {
+        console.log('beforeAll');
     })
-    test('+2', () => {
-        console.log("+2");
-        conter.addTwo()
-        expect(conter.number).toBe(2)
+    
+    afterAll(() => {
+        console.log('afterAll');
+    })
+    
+    // 每次执行测试用例前都会执行此钩子函数
+    beforeEach(() => {
+        conter = new Conter();
+        console.log('beforeEach');
+    })
+    
+    // 每次执行测试用例前都会执行此钩子函数
+    afterEach(() => {
+        console.log('afterEach');
+    })
+
+
+    describe('加法相关', () => {
+        console.log('discribe 2');
+
+        beforeEach(() =>{
+            console.log('beforeEach test add');
+        })
+        // test.only('+1', () => {
+        //     console.log("+1");
+        //     conter.addOne()
+        //     expect(conter.number).toBe(1)
+        // })
+
+        test('+1', () => {
+            console.log("+1");
+            conter.addOne()
+            expect(conter.number).toBe(1)
+        })
+        test('+2', () => {
+            console.log("+2");
+            conter.addTwo()
+            expect(conter.number).toBe(2)
+        })
+    })
+    
+    describe('减法相关', () => {
+        console.log('discribe 3');
+
+        test('-1', () => {
+            console.log('-1');
+            conter.minuOne();
+            expect(conter.number).toBe(-1)
+        })
+        test('-2', () => {
+            console.log('-2');
+            conter.minuTwo();
+            expect(conter.number).toBe(-2)
+        })
     })
 })
 
-describe('减法相关', () => {
-    test('-1', () => {
-        console.log('-1');
-        conter.minuOne();
-        expect(conter.number).toBe(-1)
-    })
-    test('-2', () => {
-        console.log('-2');
-        conter.minuTwo();
-        expect(conter.number).toBe(-2)
-    })
-})
-
-
-
-
-beforeAll(() => {
-    console.log('beforeAll');
-})
-
-afterAll(() => {
-    console.log('afterAll');
-})
-
-// 每次执行测试用例前都会执行此钩子函数
-beforeEach(() => {
-    conter = new Conter();
-    console.log('beforeEach');
-})
-
-// 每次执行测试用例前都会执行此钩子函数
-afterEach(() => {
-    console.log('afterEach');
-})
